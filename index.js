07b9e80989e5843daba1fcc724085cab6ff1a466
DannyLenk <danylenko.14@gmail.com>
2021-12-15 16:03:29 +0200

    updated index.js
    built the game for 1 player

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
 "use strict"
 
+window.onload = function() {
+   document.querySelector('#rollBtn').addEventListener('click', controller.rollDice);
+}
+
+let view = {
+   displayResult: function(player) {
+      const dice = document.getElementsByClassName('dice');
+      dice[player].innerHTML = model.players[player].dice;
+   },
+
+   displayScore: function(player) {
+      const scoreboardList = document.getElementsByClassName('scoreboard');
+      scoreboardList[player].innerHTML = model.players[player].score;
+   }
+
+};
+
+let model = {
+
+   players: [
+      {score: 0, turn: true, dice: 0},
+      {score: 0, turn: false, dice: 0}
+   ],
+
+   getRandomNum: function() {
+      return Math.floor(Math.random() * 6 + 1);
+   },
+
+   sumPlayerScore: function(player) {
+      let dice = this.getRandomNum();
+      this.players[player].dice = dice;
+      this.players[player].score += dice;
+   }
+};
+
+let controller = {
+   rollDice: function() {
+      model.sumPlayerScore(0);
+      view.displayResult(0);
+      view.displayScore(0);
+   }
+};
